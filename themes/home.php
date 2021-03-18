86b3d666ded7e9eca1c518c32cfa9b77f1cb0222
nguyenquy42 <nguyenquy26012014@gmail.com>
2021-03-18 17:55:19 +0700

    up html css about_contact_top

--- themes/home.php
+++ themes/home.php
@@ -1,18 +1,41 @@
 <main>
-  <div class="container">
-    <div class="row">
-      <div class="col-4">
-        <div class="b1"></div>
-      </div>
-      <div class="col-4">
-        <div class="">
-          <i class="fi fi-rr-following"></i>
-          <span class="fi fi-rr-gym">
+  <section class="">
+    <div class="b1"><img class="w-100" src="https://vesinhcongnghiep.com/uploads/files/2020/08/11/BANNERWEB9.jpg" alt="img-banner"></div>
+  </section>
+  <section class="section_contact_top position-relative">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="d-flex align-items-center">
+            <div class="img_contact_top">
+              <i class="fi fi-rr-envelope"></i>
+            </div>
+            <div class="content_contact_top">
+              <p class="title_c_top">Liên hệ nhanh</p>
+              <span>Phone: 0936750009</span> <br>
+              <span>Email: vesinhcongnghiep.com</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="d-flex align-items-center">
+            <div class="img_contact_top">
+              <i class="fi fi-rr-location-alt"></i>
+            </div>
+            <div class="content_contact_top">
+              <p class="title_c_top">Địa chỉ</p>
+              <span>Số 17 Mai Chí Thọ, <br> Phường Bình Khánh, Quận 2, TPHCM</span> <br>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="contact_about_top">
+              <p>Đi đầu trong dịch vụ làm sạch khu công nghiệp, cam kết hài lòng 100%</p>
+              <span>Chuyên cung cấp dịch vụ vệ sinh công nghiệp tại TP Hồ Chí Minh và các tỉnh thành lân cận với đội ngũ chuyên viên kinh nghiệm</span> <br>
+              <a class="btn__link mt-2" href="#">Xem Thêm <i class="fi fi-rr-arrow-small-right"></i></a>
+          </div>
         </div>
       </div>
-      <div class="col-4">
-        <div class="b1"></div>
-      </div>
     </div>
-  </div>
+  </section>
 </main>
